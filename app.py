62b781cce528cc78b70dca83b15ba95c34733d8c
Harrison Schumann <harrisonschumann@chevron.com>
2024-12-22 07:52:34 -0600

    updates

--- app.py
+++ app.py
@@ -1,63 +1,41 @@
 import streamlit as st
 import cv2
-import onnxruntime as ort
 import tempfile
-import os
-import numpy as np
 from ultralytics import YOLO
 import time
 
-
 def process_video(video_path, model, selected_ind):
-    videocapture = cv2.VideoCapture(video_path)  # Capture the video
+    videocapture = cv2.VideoCapture(video_path)  
     if not videocapture.isOpened():
         st.error("Could not open webcam.")
 
     fps_display = st.sidebar.empty() 
-    stop_button = st.button("Stop")  
+    stop_button = st.button("Stop Demo")  
     while videocapture.isOpened():
         success, frame = videocapture.read()
         if not success:
-            st.warning("Failed to read frame from webcam. Please make sure the webcam is connected properly.")
+            st.warning("Failed to read frame from webcam...")
             break
 
-        prev_time = time.time()  # Store initial time for FPS calculation
-
-        # Store model predictions
-        # if enable_trk == "Yes":
-        #     results = model.track(frame, conf=conf, iou=iou, classes=selected_ind, persist=True)
-        # else:
+        prev_time = time.time()  
         results = model(frame, conf=conf, iou=iou, classes=selected_ind)
-        annotated_frame = results[0].plot()  # Add annotations on frame
-
-
-        # Calculate model FPS
+        annotated_frame = results[0].plot()  
         curr_time = time.time()
         fps = 1 / (curr_time - prev_time)
 
-        # display frame
         org_frame.image(frame, channels="BGR")
         ann_frame.image(annotated_frame, channels="BGR")
 
         if stop_button:
-            videocapture.release()  # Release the capture
-            torch.cuda.empty_cache()  # Clear CUDA memory
-            st.stop()  # Stop streamlit app
+            videocapture.release()  
+            st.stop()  
 
-        # Display FPS in sidebar
         fps_display.metric("FPS", f"{fps:.2f}")
 
-    # Release the capture
     videocapture.release()
-
-    # Clear CUDA memory
-    torch.cuda.empty_cache()
-
-    # Destroy window
     cv2.destroyAllWindows()
 
 def process_image(image_path, model, selected_ind):
-
     results = model([image_path], conf=conf, iou=iou, classes=selected_ind)
     # Process results list
     for result in results:
@@ -66,31 +44,38 @@ def process_image(image_path, model, selected_ind):
         
 
 if __name__=="__main__":
-    # Streamlit UI
     st.title("Object Detection Demo")
 
     model_selection = st.sidebar.selectbox(
         "Model",
-        ("yolov11_out_of_the_box", "yolov11_aircraft_detect"),
+        ("yolov11_aircraft_detect", "yolov11_out_of_the_box"),
     )
+
+    model_size = st.sidebar.selectbox(
+        "Model Size",
+        ("nano", "small", "medium"),
+    )
+
     if model_selection == "yolov11_aircraft_detect":
-        model_path = "./runs/detect/train/weights/best.onnx"
+        model_path = f"./{model_size}/runs/detect/train/weights/best.onnx"
         model = YOLO(model_path)
         class_names = list(model.names.values())  
-        selected_classes = st.sidebar.multiselect("Classes", ["all"] + class_names, default="all")
-        if selected_classes == ["all"]:
-            selected_ind = list(range(0, len(class_names)))
-        else:
-            selected_ind = [class_names.index(option) for option in selected_classes]
+
     elif model_selection == "yolov11_out_of_the_box":
-        model_path = "yolo11m.onnx"
+        suffix_map = {
+            "nano":"n",
+            "small":"s",
+            "medium":"m"
+        }
+        model_path = f"{model_size}/yolo11{suffix_map[model_size]}.onnx"
         model = YOLO(model_path)
         class_names = list(model.names.values())  
-        selected_classes = st.sidebar.multiselect("Classes", ["all"] + class_names, default="all")
-        if selected_classes == ["all"]:
-            selected_ind = list(range(0, len(class_names)))
-        else:
-            selected_ind = [class_names.index(option) for option in selected_classes]
+
+    selected_classes = st.sidebar.multiselect("Classes", ["all"] + class_names, default="all")
+    if "all" in selected_classes:
+        selected_ind = list(range(0, len(class_names)))
+    else:
+        selected_ind = [class_names.index(option) for option in selected_classes]
 
     source = st.sidebar.selectbox(
         "Source",
@@ -100,57 +85,21 @@ if __name__=="__main__":
     conf = float(st.sidebar.slider("Confidence Threshold", 0.0, 1.0, 0.25, 0.01))
     iou = float(st.sidebar.slider("IoU Threshold", 0.0, 1.0, 0.45, 0.01))
 
-    if source == "video":
-        vid_file = st.sidebar.file_uploader("Upload Video File", type=["mp4", "mov", "avi", "mkv"])
-        if vid_file:
-            with st.spinner("Processing video..."):
-                with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
-                    temp_file.write(vid_file.read())
-                    video_path = temp_file.name
-    elif source == "webcam":
-        video_path = 0
-
-    elif source == "image":
-        img_file = st.sidebar.file_uploader("Upload Image File", type=["jpeg", "jpg", "png"])
-        if img_file:
+    if source in ["video", "image"]:
+        input_file = st.sidebar.file_uploader("Upload Video File", type=["mp4", "mov", "avi", "mkv"])
+        if input_file:
             with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
-                temp_file.write(img_file.read())
-                img_path = temp_file.name
+                temp_file.write(input_file.read())
+                input_file_path = temp_file.name
+    elif source == "webcam":
+        input_file_path = 0
 
     if st.button('Start Demo'):
         col1, col2 = st.columns(2)
         org_frame = col1.empty()
         ann_frame = col2.empty()
-
         if source in ["video", "webcam"]:
-            process_video(video_path=video_path, model=model, selected_ind=selected_ind)
-
+            process_video(video_path=input_file_path, model=model, selected_ind=selected_ind)
         elif source == "image":
-            process_image(img_path, model, selected_ind)
+            process_image(input_file_path, model, selected_ind)
         
-
-
-    # # File uploader
-    # onnx_path = "./runs/detect/train/weights/best.onnx"#= st.text_input("Path to YOLO ONNX Model", value="yolov4.onnx")
-
-    # if uploaded_file and onnx_path:
-    #     st.write("Processing video...")
-    #     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
-    #         temp_file.write(uploaded_file.read())
-    #         video_path = temp_file.name
-
-    #     # Load model
-    #     yolo_model = load_model(onnx_path)
-        
-    #     # Process video
-    #     input_shape = (640, 640)  # Adjust based on your YOLO model
-    #     detections = process_video(video_path, yolo_model, input_shape)
-
-    #     # Display results
-    #     st.write(f"Total segments with objects: {len(detections)}")
-    #     for detection in detections:
-    #         frame_no = detection["frame"]
-    #         classes_detected = detection["classes"]
-    #         st.write(f"Frame: {frame_no}, Classes: {classes_detected}")
-
-    #     os.remove(video_path)
--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,22 +1,6 @@
-import os
-import torch
-import onnx 
-import onnxslim
+from ultralytics import YOLO
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
+model_path = "./medium/runs/detect/train/weights/best.pt"
+model = YOLO(model_path)
 
-pt_model_path = os.path.join(current_directory,"runs","detect","train","weights","best.pt")
-onnx_model_path = os.path.join(current_directory,"onnx_models","best.onnx")
-
-model = torch.load(pt_model_path)['model'].float()
-model.eval()
-
-dummy_input = torch.randn(1,3,640,640)
-
-torch.onnx.export(model, dummy_input, onnx_model_path)
-
-model_onnx = onnx.load(onnx_model_path)
-model_onnx = onnxslim.slim(model_onnx)
-onnx.save(model_onnx, onnx_model_path)
-
-
+model.export(format="onnx") 
